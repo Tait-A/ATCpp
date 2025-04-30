062da5fda4220efa2b444a675d740b3930d54c38
Tait-A <s2051131@ed.ac.uk>
2025-04-30 07:34:41 +0100

    some type_names

--- include/type_templates.h
+++ include/type_templates.h
@@ -1,3 +1,11 @@
+#ifndef TYPE_TEMPLATES_H
+#define TYPE_TEMPLATES_H
+
+#include <iostream>
+#include <string>
+#include <type_traits>
+#include <vector>
+
 template <typename T>
 const char* type_name() {
     return "Unknown Type";
@@ -23,3 +31,25 @@ const char* type_name<std::string>() {
     return "std::string";
 }
 
+template <typename T>
+std::enable_if_t<std::is_pointer_v<T>, const char*>
+type_name() {
+    using PointeeType = std::remove_pointer_t<T>;
+    static std::string result = std::string("Pointer to ") + type_name<PointeeType>();
+    return result.c_str();
+}
+
+template <typename T>
+const char* type_name(std::vector<T> const&) {
+    static std::string result = std::string("std::vector<") + type_name<T>() + ">";
+    return result.c_str();
+}
+
+template <typename T>
+std::enable_if_t<!std::is_same_v<typename T::value_type, void>, const char*>
+type_name(T const&) {
+    static std::string result = std::string("Unknown container of ") + type_name<typename T::value_type>();
+    return result.c_str();
+}
+
+#endif // TYPE_TEMPLATES_H
